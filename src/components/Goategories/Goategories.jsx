d785e0f771b8c0a6a80782acd96c90d6851861c7
Aaron Wolfe <3wolfeaaron5@gmail.com>
2019-08-29 15:59:40 -0500

    adds edit and add function

--- src/components/Goategories/Goategories.jsx
+++ src/components/Goategories/Goategories.jsx
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 
 class Categories extends Component {
-  state = {  }
+  state = {
+    newCategory: '',
+    editMode: false
+  }
 
-  componentDidMount(){
+  componentDidMount() {
     this.props.dispatch({
       type: 'FETCH_CATEGORIES'
     })
@@ -17,21 +20,65 @@ class Categories extends Component {
     })
   }
 
-  render() { 
-    return ( 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (!this.state.editMode) {
+      this.props.dispatch({
+        type: 'ADD_CATEGORY',
+        payload: this.state.newCategory
+      })
+    } else {
+      this.props.dispatch({
+        type: 'EDIT_GOATEGORY',
+        payload: this.state.newCategory
+      })
+    }
+    this.setState({
+      newCategory: '',
+      editMode: false
+    })
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      newCategory: event.target.value,
+      editMode: this.state.editMode
+    })
+  }
+
+  handleEdit = (name) => {
+    this.setState({
+      newCategory: name,
+      editMode: !this.state.editMode
+    })
+  }
+
+  exitEdit = () => {
+    this.setState({
+      newCategory: '',
+      editMode: false
+    })
+  }
+
+  render() {
+    return (
       <div>
         <h1>List of Categories</h1>
-        <form>
-          <input></input> 
-          <button>Add Category</button>
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleChange} placeholder='Name' value={this.state.newCategory}></input>
+          <button>{this.state.editMode ? 'Edit Category': 'Add Category'}</button>
+          {this.state.editMode && (<button onClick={this.exitEdit}>Exit Edit</button>)}
         </form>
         <ul>
-          {this.props.reduxStore.categoryReducer.map(cat => 
-          <li key={cat.id}>{cat.name} <span> </span> <button onClick={()=>this.handleClick(cat.id)}>Delete</button></li>
+          {this.props.reduxStore.categoryReducer.map(cat =>
+            <li key={cat.id}>
+              {cat.name} <span> </span> <button onClick={() => this.handleClick(cat.id)}>Delete</button>
+              <button onClick={()=>this.handleEdit(cat.name)}>Edit</button>
+            </li>
           )}
         </ul>
       </div>
-     );
+    );
   }
 }
 
@@ -40,5 +87,5 @@ const mapStoreToProps = (reduxStore) => {
     reduxStore,
   }
 }
- 
+
 export default connect(mapStoreToProps)(Categories);

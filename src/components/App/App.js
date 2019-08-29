4fb7d276ac7251de442fae6ca78abb45bda8c8bf
Aaron Wolfe <3wolfeaaron5@gmail.com>
2019-08-29 15:32:29 -0500

    adds delete route for goategories

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Favorites from '../Favorites/Favorites'
 import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import Search from '../Search/Search';
+import Categories from '../Goategories/Goategories';
 
 
 class App extends Component {
@@ -17,11 +18,15 @@ class App extends Component {
           <li>
             <Link to = "/favorites">Favorites</Link>
           </li>
+            <li>
+            <Link to="/categories">Categories</Link>
+          </li>
         </ul>
         <h1>Giphy Search!</h1>
       </div>
       <Route exact path="/" component={Search} />
       <Route path="/favorites" component={Favorites}/>
+      <Route path="/categories" component={Categories} />
       </Router>
     );
   }

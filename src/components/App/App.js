caaf0a1f8a9058315987aceb28d114110e0f1867
Christopher O'Bannon <Crobwan@gmail.com>
2019-08-29 11:07:02 -0500

    creates Router in App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import {Hashrouter as Router, Route, Link} from 'react-router-dom';
 
 
 class App extends Component {
 
   render() {
     return (
+      <Router>
       <div>
+        <ul>
+          <li>
+            <Link to="/">Search</Link>
+          </li>
+          <li>
+            <Link to = "/favorites">Favorites</Link>
+          </li>
+        </ul>
         <h1>Giphy Search!</h1>
-      </div>
+      </div> 
+      <Route path="/" Component={Search} />
+      <Route path="/favorites" component={Favorites}/>
+      </Router>
     );
   }
   
